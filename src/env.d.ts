d786c2b9fb64fef0494e09a2733966b412bd3dbb
Alex Van Maele <alexander.vanmaele@vlaanderen.be>
2026-07-04 12:24:06 +0200

    feat: add TypeScript definitions for Astro client and JSX namespace

---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="astro/client" />
+/// <reference path="../.astro/types.d.ts" />
+
+declare namespace JSX {
+  interface IntrinsicElements extends astroHTML.JSX.IntrinsicElements {}
+}
+
